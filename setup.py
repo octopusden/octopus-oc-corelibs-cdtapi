b8f99544e20fe8c1d21c31b2b44a2c3443c9bced
Aleksey Knyazev <ctj_yebbs@inbox.ru>
2023-05-22 12:11:39 +0300

    Fixed "python_requires" for PyPI metadata compatibility (#11)

--- setup.py
+++ setup.py
@@ -3,14 +3,11 @@
 from setuptools import setup
 from sys import version_info
 
-__version = "3.9.1"
+__version = "3.9.2"
 install_requires = ["requests"]
 tests_require = []
 
-if version_info.major >= 3:
-    python_requires = ">=3.6"
-else:
-    python_requires = ">=2.7,<3"
+if version_info.major < 3:
     # these modlues comes with 3.6 and later intepreters but not included in 2.7
     install_requires.append("mock==2.0.0") # needed for tests only but can not be installed with 'pip' if not specified here
     install_requires.append("enum")
@@ -25,7 +22,7 @@ spec = {
     "packages": ["oc_cdtapi"],
     "install_requires": install_requires,
     "tests_require": tests_require,
-    "python_requires": python_requires,
+    "python_requires": ">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*",
     "scripts": [
         "nexus.py"
     ],
