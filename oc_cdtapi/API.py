a68e4632e22d9453df0ceb08ff825410e67c29c0
Aleksey Knyazev <ctj_yebbs@inbox.ru>
2023-06-02 11:50:06 +0300

    doc added

--- oc_cdtapi/API.py
+++ oc_cdtapi/API.py
@@ -10,8 +10,11 @@ import sys
 
 if sys.version_info.major == 2:
     strtype = basestring
+    import urlparse
 elif sys.version_info.major == 3:
     strtype = str
+    import urllib.parse as urlparse
+
 
 class HttpAPIError(Exception):
 
@@ -20,7 +23,6 @@ class HttpAPIError(Exception):
         self.url = url
         self.resp = resp
         self.text = text
-
 
     def __str__(self):
         return self.text + ': Code ' + str(self.code) + ' ' + self.url
@@ -40,11 +42,11 @@ class HttpAPI(object):
 
     def __init__(self, root=None, user=None, auth=None, readonly=False, anonymous=False):
         """
-        :param root: Root URL (uses *_URL by default)
-        :param user: Username (uses *_USER by default)
-        :param auth: Password (uses *_PASSWORD by default)
-        :param readonly: sets readonly property, should be explictly supported by child 
-        :param anonymous: ignore username/password provided and use anonymous requests
+        :param str root: Root URL (uses *_URL by default)
+        :param str user: Username (uses *_USER by default)
+        :param str auth: Password (uses *_PASSWORD by default)
+        :param bool readonly: sets readonly property, should be explictly supported by child 
+        :param bool anonymous: ignore username/password provided and use anonymous requests
 
         >>> import os
         >>> from oc_cdtapi.API import HttpAPI
@@ -67,30 +69,64 @@ class HttpAPI(object):
         """
 
         self.readonly = readonly
-        if self._env_prefix != None:
-            if not root: root = os.getenv(self._env_prefix + self._env_url)
-            if not user: user = os.getenv(self._env_prefix + self._env_user)
-            if not auth: auth = os.getenv(self._env_prefix + self._env_auth)
-        if not root: raise ValueError('Server URL for service [%s] not set' % self._env_prefix)
+
+        if self._env_prefix is not None:
+            if not root:
+                root = os.getenv(self._env_prefix + self._env_url)
+
+            if not user:
+                user = os.getenv(self._env_prefix + self._env_user)
+
+            if not auth:
+                auth = os.getenv(self._env_prefix + self._env_auth)
+
+        if not root:
+            raise ValueError('Server URL for service [%s] not set' % self._env_prefix)
+
+        # check we have correct schema
+        _scheme = urlparse.urlparse(root).scheme
+
+        if not _scheme.startswith("http"):
+            raise ValueError('URL scheme is not supported for "%s"' % root)
+
         self.root = root
         self.web = requests.Session()
-        if user != None and not anonymous: self.web.auth = (user, auth)
 
-        if self.root.startswith( "https:" ):
-            self.web.verify = False;
+        if user and not anonymous:
+            self.web.auth = (user, auth)
 
+        if _scheme == "https":
+            # ignore self-signed certificates warning
+            self.web.verify = False
 
     def set_readonly(self, readonly=True):
+        """
+        Set instance as read-only
+        :param bool readonly: foo
+        """
         self.readonly = readonly
 
-
     def re(self, req):
-        """ Constructs request URL from request name """
-        return posixpath.join(self.root, req)
+        """ 
+        Constructs request URL from request name
+        :param str req: string or list of strings for the request
+        """
+        # do not use 'urlparse.urljoin' here because it gives wrong result if 'root'
+        # contains sub-path. Example:
+        # urlparse.urljoin("https://exapmle.com:5400/c1/c2/c3", "c4/c5/c6")
+        # gives 'https://exapmle.com:5400/c1/c2/c4/c5/c6'
+        # while 'https://exapmle.com:5400/c1/c2/c3/c4/c5/c6' expected ('c3' missing)
+        if isinstance(req, list):
+            req = posixpath.sep.join(req)
 
+        return posixpath.join(self.root, req)
 
     def pp(self, resp, write_to=None, stream=False, **kvarg):
         """ Post-processes response
+        :param requests.Response resp: the response object
+        :param fileObj write_to: file object to write result to
+        :param bool stream: use stream mode (useful for large objects)
+        :param kvarg: another keyword arguments, not actually used
 
         New feature! If write_to parameter is given to any method, that returns response
         object, request result will be written to fd or filename specified in write_to.
@@ -147,29 +183,50 @@ class HttpAPI(object):
 
         if resp.status_code < self.raise_exception_low or resp.status_code > self.raise_exception_high:
             raise self._error(resp.status_code, resp.url, resp, 'Error making request to server')
-        if write_to != None:
-            if isinstance (write_to, strtype):
+
+        if write_to is not None:
+
+            if isinstance(write_to, strtype):
                 fd = open(write_to, 'wb')
             else:
                 fd = write_to
+
             if not stream:
                 fd.write(resp.content)
             else:
                 shutil.copyfileobj(resp.raw, fd)
+
             fd.flush()
-            if isinstance (write_to, strtype): fd.close()
+
+            if isinstance(write_to, strtype):
+                fd.close()
+
             del fd
+
         return resp
 
-
     def __kvarg(self, kvarg):
-        kvarg = kvarg.copy()
-        if 'write_to' in kvarg: del kvarg['write_to']
-        return kvarg
+        """
+        Omit 'write_to' argument
+        :param dict kvarg: keyword arguments
+        :return dict: ditionary without 'write_to' key
+        """
 
+        kvarg = kvarg.copy()
+        if 'write_to' in kvarg:
+            del kvarg['write_to']
+
+        return kvarg
 
     def get(self, req, params=None, files=None, data=None, headers=None, **kvarg):
         """Sends GET request
+        :param str req: request sub-URL
+        :param dict params: additional GET parameters
+        :param files: files to append to the request
+        :param data: additional data to append to the request
+        :param dict headers: additional headers for the request
+        :param kvarg: additional keyword arguments
+        :return requests.Response: postprocessed the response object
 
         >>> def test(port):
         ...     api = HttpAPI('http://127.0.0.1:' + str(port))
@@ -193,18 +250,36 @@ class HttpAPI(object):
 
         """
 
-        resp = self.web.get(self.re(req), params=params, data=data, files=files, headers=headers, **self.__kvarg(kvarg))
-        return self.pp(resp, **kvarg)
+        resp = self.web.get(self.re(req), params=params, data=data,
+                            files=files, headers=headers, **self.__kvarg(kvarg))
 
+        return self.pp(resp, **kvarg)
 
     def post(self, req, params=None, files=None, data=None, headers=None, **kvarg):
-        """Sends POST request"""
-        resp = self.web.post(self.re(req), params=params, data=data, files=files, headers=headers, **kvarg)
+        """
+        Sends POST request
+        :param str req: request sub-URL
+        :param dict params: additional GET parameters
+        :param files: files to append to the request
+        :param data: additional data to append to the request
+        :param dict headers: additional headers for the request
+        :param kvarg: additional keyword arguments
+        :return requests.Response: postprocessed the response object
+        """
+        resp = self.web.post(self.re(req), params=params,
+                             data=data, files=files, headers=headers, **kvarg)
         return self.pp(resp, **kvarg)
-
 
     def put(self, req, params=None, files=None, data=None, headers=None, **kvarg):
         """Sends PUT request
+        :param str req: request sub-URL
+        :param dict params: additional GET parameters
+        :param files: files to append to the request
+        :param data: additional data to append to the request
+        :param dict headers: additional headers for the request
+        :param kvarg: additional keyword arguments
+        :return requests.Response: postprocessed the response object
+
         >>> def test(port):
         ...     api = HttpAPI('http://127.0.0.1:' + str(port))
         ...     api.put('getrequest')
@@ -215,35 +290,64 @@ class HttpAPI(object):
 
         """
 
-        resp = self.web.put(self.re(req), params=params, data=data, files=files, headers=headers, **kvarg)
+        resp = self.web.put(self.re(req), params=params,
+                            data=data, files=files, headers=headers, **kvarg)
         return self.pp(resp, **kvarg)
 
-
     def delete(self, req, params=None, files=None, data=None, headers=None, **kvarg):
-        """Sends DELETE request"""
-        resp = self.web.delete(self.re(req), params=params, data=data, files=files, headers=headers, **kvarg)
+        """
+        Sends DELETE request
+        :param str req: request sub-URL
+        :param dict params: additional GET parameters
+        :param files: files to append to the request
+        :param data: additional data to append to the request
+        :param dict headers: additional headers for the request
+        :param kvarg: additional keyword arguments
+        :return requests.Response: postprocessed the response object
+        """
+        resp = self.web.delete(self.re(req), params=params,
+                               data=data, files=files, headers=headers, **kvarg)
         return self.pp(resp, **kvarg)
 
     def head(self, req, params=None, files=None, data=None, headers=None, **kvarg):
-        """Sends HEAD request"""
-        resp = self.web.head(self.re(req), params=params, data=data, files=files, headers=headers, **kvarg)
+        """
+        Sends HEAD request
+        :param str req: request sub-URL
+        :param dict params: additional GET parameters
+        :param files: files to append to the request
+        :param data: additional data to append to the request
+        :param dict headers: additional headers for the request
+        :param kvarg: additional keyword arguments
+        :return requests.Response: postprocessed the response object
+        """
+        resp = self.web.head(self.re(req), params=params,
+                             data=data, files=files, headers=headers, **kvarg)
         return self.pp(resp, **kvarg)
 
 
 # Two shortcuts to deal with XML without having full XML support
 def get_xml_tag(config, tag):
-    """Gets tag value from xml config"""
+    """
+    Gets tag value from xml config
+    Warning: this method ignores XML-comments and returns the first inclusion only
+    :param str config: xml string
+    :param str tag: tag to get
+    :return str: first tag value found
+    """
     return ((config.split('<' + tag + '>')[1]).split('</' + tag + '>')[0])
 
 
 def edit_xml_tag(config, tag, value):
-    """Sets tag value in xml config. Works fine if there is only single such tag in XML"""
+    """
+    Sets tag value in xml config. Works fine if there is only single such tag in XML
+    Warning: this method ignores XML-comments and returns the first inclusion only
+    :param str config: xml string
+    :param str tag: tag to edit
+    :param str value: value to set
+    :return str: str with replaced tag value
+    """
     config = config.split('<' + tag + '>')[0] + '<' + tag + '>' + value + '</' + tag + '>' + \
-             config.split('</' + tag + '>')[1]
+        config.split('</' + tag + '>')[1]
+
     return config
 
-
-if __name__ == "__main__":
-    import doctest
-
-    doctest.testmod()
