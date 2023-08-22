1ffad5c1ccb46b0bf9c273833a89e648938afced
Aleksey Knyazev <ctj_yebbs@inbox.ru>
2023-08-22 11:29:52 +0300

    tests for DmsAPI v3

--- oc_cdtapi/tests/test_DmsAPIv3.py
+++ oc_cdtapi/tests/test_DmsAPIv3.py
@@ -15,3 +15,50 @@ class TestDmsApiV3(unittest.TestCase):
         _ret.content = 'test_content'
         self._dms.get.return_value = _ret
         self.assertEqual(self._dms.ping_dms(), _ret.content)
+        self._dms.get.assert_called_once()
+
+    def test_get_components(self):
+        _ret_dict = {"components": ["component"]}
+        _ret = unittest.mock.MagicMock()
+        _ret.json = unittest.mock.MagicMock(return_value=_ret_dict)
+        self._dms.get.return_value = _ret
+        self.assertListEqual(_ret_dict.get("components"), self._dms.get_components())
+        self._dms.get.assert_called_once_with('components')
+
+    def test_get_versions(self):
+        _ret_dict = {"versions": [
+            {"version": "1"},
+            {"version": "2"},
+            {"version": "3"}]}
+        _ret = unittest.mock.MagicMock()
+        _ret.json = unittest.mock.MagicMock(return_value=_ret_dict)
+        self._dms.get.return_value = _ret
+        self.assertListEqual(["1", "2", "3"], self._dms.get_versions('component'))
+        self._dms.get.assert_called_once_with(['components', 'component', 'versions'])
+
+    def test_get_artifacts(self):
+        _ret_dict = {"artifacts": ["1", "2", "3"]}
+        _ret = unittest.mock.MagicMock()
+        _ret.json = unittest.mock.MagicMock(return_value=_ret_dict)
+        self._dms.get.return_value = _ret
+        self.assertListEqual(["1", "2", "3"], self._dms.get_artifacts('component', "version"))
+        self._dms.get.assert_called_once_with(["components", "component", "versions", "version", "artifacts"],
+                                              params=None)
+        self._dms.get.reset_mock()
+        self.assertListEqual(["1", "2", "3"], self._dms.get_artifacts('component', "version", "type"))
+        self._dms.get.assert_called_once_with(["components", "component", "versions", "version", "artifacts"],
+                                              params={"type": "type"})
+
+    def test_download_artifact(self):
+        _ret = "test_content"
+        self._dms.get.return_value = _ret
+        self.assertEqual(_ret, self._dms.download_artifact(1, write_to='tempfile'))
+        self._dms.get.assert_called_once_with(["artifacts", "1", "download"], stream=True, write_to="tempfile")
+
+    def test_download_component(self):
+        _ret = "test_content"
+        self._dms.get.return_value = _ret
+        self.assertEqual(_ret, self._dms.download_component("component", "version", 1, write_to='tempfile'))
+        self._dms.get.assert_called_once_with([
+            "components", "component", "versions", "version", "artifacts", "1", "download"],
+                                              stream=True, write_to="tempfile")
