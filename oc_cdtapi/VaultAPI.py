e1858fe75f66b92bdda5eeb90eb17c73da75a24c
bl1nkker <nurovich14@gmail.com>
2025-12-15 12:51:42 +0500

    feat: updated naming convention + secret retrievement

--- oc_cdtapi/VaultAPI.py
+++ oc_cdtapi/VaultAPI.py
@@ -1,33 +1,32 @@
+import re
 import logging
 import os
+from typing import Any
 
 import hvac
+import requests
 from hvac.exceptions import VaultError
+
+SECRET_PATTERN = re.compile(r"^[A-Z][A-Z0-9_]*__[A-Z][A-Z0-9_]*$")
+
 
 class VaultAPI:
     def __init__(self,
-                 vault_enable=False,
                  vault_url=None,
                  vault_token=None,
                  vault_mount_point=None,
                  verify_ssl=True):
-        self.vault_enable = vault_enable or os.getenv("VAULT_ENABLE")
         self.vault_url = vault_url or os.getenv("VAULT_URL")
         self.vault_token = vault_token or os.getenv("VAULT_TOKEN")
         self.mount_point = vault_mount_point or os.getenv("VAULT_MOUNT_POINT")
-        self.use_staging_secrets = os.getenv("USE_STAGING_ENVIRONMENT", "false").lower() == "true" #Check whether we have env USE_STAGING_ENVIRONMENT true or not
         self.verify_ssl = verify_ssl
         self._client = None
 
         # Create a logger instance for this class
         self.logger = logging.getLogger(__name__)
-        
-    @property
-    def client(self):
-        if not self.vault_enable:
-            self.logger.warning("VAULT_ENABLE environment set to false, skip using vault")
-            return None
 
+    @property
+    def client(self) -> hvac.Client | None:
         if self._client is None:
             if not self.vault_url:
                 self.logger.warning("VAULT_URL environment variable or vault_url parameter is missing, skip using vault")
@@ -41,39 +40,57 @@ class VaultAPI:
                 token=self.vault_token,
                 verify=self.verify_ssl
             )
-
-            if not self._client.is_authenticated():
+            try:
+                is_authenticated = self._client.is_authenticated()
+            except requests.exceptions.ConnectionError as e:
+                self.logger.warning(f"Failed to authenticate with Vault - Vault is unreachable: {e}")
+                self._client = None
+                return None
+            if not is_authenticated:
                 self.logger.warning("Failed to authenticate with Vault - check credentials, skip using vault")
                 return None
-
         return self._client
 
-    def parse_secret_name(self, name):
-        if 'USER' in name:
-            split_name = name.split('_USER')[0]
-            return split_name, 'USER'
+    def _parse_secret_name(self, name: str) -> list[str]:
+        if not SECRET_PATTERN.match(name):
+            raise ValueError("Secret name must match <PATH>__<KEY>")
 
-        if 'PASSWORD' in name:
-            split_name = name.split('_PASSWORD')[0]
-            return split_name, 'PASSWORD'
+        return name.split("__", 1)
 
-        return 'OTHER', name
-
-    def get_secret_from_path(self, name):
+    def get_secret_from_path(self, name: str) -> Any | None:
         client = self.client
         if client is None:
             return None
-
-        secret_path, credentials = self.parse_secret_name(name)
-        if self.use_staging_secrets:
-            secret_path = secret_path + "_TEST"
+        try:
+            secret_path, credentials = self._parse_secret_name(name=name)
+        except ValueError as e:
+            self.logger.warning(f"Failed parsing secret: {e}")
+            return None
 
         try:
             response = client.secrets.kv.read_secret_version(path=secret_path, mount_point=self.mount_point)
-            return response['data']['data'].get(credentials)
+            return response["data"]["data"].get(credentials)
         except VaultError as e:
             self.logger.warning(f"Failed getting data from vault for path {secret_path} and credentials {credentials}: {e}")
             return None
+        except requests.exceptions.ConnectionError as e:
+            self.logger.warning(f"Failed to authenticate with Vault - Vault is unreachable: {e}")
+            return None
 
-    def load_secret(self, name, default=None):
-        return self.get_secret_from_path(name) or os.getenv(name, default)
+    def load_secret(self, name: str, defaults: dict[str, Any] | None = None) -> Any | None:
+        def _get_from_sources(key: str) -> Any | None:
+            for src in (os.environ, defaults):
+                if src is not None and key in src:
+                    return src[key]
+            return None
+
+        is_test = _get_from_sources("PYTHON_ENV") == "test"
+
+        if is_test:
+            name = f"{name}_TEST"
+
+        value = self.get_secret_from_path(name=name)
+        if value is not None:
+            return value
+
+        return _get_from_sources(name)
